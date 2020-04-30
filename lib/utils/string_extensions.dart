aaee578f16000d4a8fc7f1c48e85a9b86526e4fc
gen <i8badf00d@gmail.com>
2020-04-30 16:42:51 +0000

    リファクタ

---
+++ lib/utils/string_extensions.dart
@@ -0,0 +1,3 @@
+extension StringExtension on String {
+  int toInt() => int.tryParse(this);
+}
--- lib/widget/thread_grid_cell.dart
+++ lib/widget/thread_grid_cell.dart
@@ -9,52 +9,49 @@ class ThreadGridCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => onTap?.call(),
-      child: Stack(
-        children: [
-          // サムネイル画像
-          SizedBox.expand(
-            child: thread.thumbnailUrl != null
-                ? Image.network(
-                    thread.thumbnailUrl.toString(),
-                  )
-                : const Icon(Icons.broken_image),
+    return Stack(
+      children: [
+        // サムネイル画像
+        SizedBox.expand(
+          child: thread.thumbnailUrl != null
+              ? Image.network(
+                  thread.thumbnailUrl.toString(),
+                )
+              : const Icon(Icons.broken_image),
+        ),
+        // レス数
+        SizedBox(
+          child: Container(
+            padding: const EdgeInsets.all(2),
+            color: Colors.black54,
+            child: Text(
+              '${thread.replyCount}',
+              style: const TextStyle(color: Colors.white),
+            ),
           ),
-          // レス数
-          SizedBox(
+        ),
+        // 本文
+        Positioned(
+          left: 0,
+          right: 0,
+          bottom: 0,
+          child: SizedBox(
             child: Container(
-              padding: const EdgeInsets.all(2),
+              padding: const EdgeInsets.only(left: 2, right: 2),
               color: Colors.black54,
               child: Text(
-                '${thread.replyCount}',
+                '${thread.body}',
+                overflow: TextOverflow.ellipsis,
                 style: const TextStyle(color: Colors.white),
               ),
             ),
           ),
-          // 本文
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: SizedBox(
-              child: Container(
-                padding: const EdgeInsets.only(left: 2, right: 2),
-                color: Colors.black54,
-                child: Text(
-                  '${thread.body}',
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ),
-          Material(
-            type: MaterialType.transparency,
-            child: InkWell(onTap: () => onTap?.call()),
-          ),
-        ],
-      ),
+        ),
+        Material(
+          type: MaterialType.transparency,
+          child: InkWell(onTap: () => onTap?.call()),
+        ),
+      ],
     );
   }
 }
