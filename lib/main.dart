14538b75a073fc28500f90f77db74eaeb6119efc
gen <i8badf00d@gmail.com>
2020-05-03 17:02:05 +0000

    フォント追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:futaba_client/page/home/home_page.dart';
 import 'package:futaba_client/store/catalog_sort_type_store.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('fonts/NotoSansJP/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['Noto Sans JP'], license);
+  });
+
   runApp(
     ChangeNotifierProvider<CatalogSortTypeStore>(
       create: (_) => CatalogSortTypeStore(),
@@ -18,9 +25,22 @@ class MyApp extends StatelessWidget {
     const appTitle = 'ふたば';
     return MaterialApp(
       title: appTitle,
-      theme: ThemeData.from(colorScheme: const ColorScheme.light()),
-      darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark()),
+      theme: ThemeData.from(colorScheme: const ColorScheme.light())
+          .apply(fontFamily: 'NotoSansJP'),
+      darkTheme: ThemeData.from(colorScheme: const ColorScheme.dark())
+          .apply(fontFamily: 'NotoSansJP'),
       home: const HomePage(title: appTitle),
+    );
+  }
+}
+
+extension ThemeDataExtension on ThemeData {
+  ThemeData apply({@required String fontFamily}) {
+    assert(fontFamily != null);
+    return copyWith(
+      textTheme: textTheme.apply(fontFamily: fontFamily),
+      accentTextTheme: accentTextTheme.apply(fontFamily: fontFamily),
+      primaryTextTheme: primaryTextTheme.apply(fontFamily: fontFamily),
     );
   }
 }
